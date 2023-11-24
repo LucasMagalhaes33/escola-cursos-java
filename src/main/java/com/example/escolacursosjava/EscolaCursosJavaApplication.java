fdac7b4ea12b1f20b814d2cf61aeb1209bc90802
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-11-23 23:10:19 -0300

    Initial commit

---
+++ src/main/java/com/example/escolacursosjava/EscolaCursosJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.escolacursosjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaCursosJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscolaCursosJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/escolacursosjava/EscolaCursosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.escolacursosjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaCursosJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
