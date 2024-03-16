4d52635e109dfcfa6df523444e80ffa77878bfc6
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2024-03-16 20:34:38 -0300

    add: mapping lessons to courses.

--- src/main/java/com/example/escolacursosjava/model/Lesson.java
+++ src/main/java/com/example/escolacursosjava/model/Lesson.java
@@ -1,4 +1,24 @@
 package com.example.escolacursosjava.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 15, nullable = false)
+    private String youtubeUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
 }
