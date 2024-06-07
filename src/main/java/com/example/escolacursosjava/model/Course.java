85665ea8551d4d36d3233078e13bd83ce29b5fbf
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2024-06-07 14:52:55 -0300

    feat: add model, service, resource and notations to deploy on aws

--- src/main/java/com/example/escolacursosjava/model/Course.java
+++ src/main/java/com/example/escolacursosjava/model/Course.java
@@ -10,8 +10,10 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
@@ -23,7 +25,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@SQLDelete(sql = "UPDATE course SET status = 'Inativo' WHERE id = ?")
 @Where(clause = "status = 'Ativo'")
 @Entity
 public class Course {
